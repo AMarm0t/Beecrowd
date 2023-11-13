c18c0093dd0ca3431face1dd6444fd7de8264f9a
AMarm0t <150290285+AMarm0t@users.noreply.github.com>
2023-11-13 20:51:36 +0600

    Add files via upload

---
+++ 1217.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+#include<cctype>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    int t;
+    float total_price=0,total_count=0;
+    cin>>t;
+    cin.ignore();
+    for(int i=1;i<=t;i++)
+    {
+        float price,count=1;
+         string name;
+        cin>>price;
+        cin.ignore();
+        getline(cin,name);
+        for(int j=0;j<name.size();j++)
+        {
+            if(isalpha(name[j])&&name[j-1]==' ')
+                count++;
+        }
+         total_count+=count;
+         total_price+=price;
+        cout<<"day "<<i<<": "<<count<<" kg"<<endl;
+    }
+    cout<<fixed<<setprecision(2)<<total_count/t<<" kg by day"<<endl;
+    cout<<fixed<<"R$ "<<setprecision(2)<<total_price/t<<" by day"<<endl;
+}
